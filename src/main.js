7bc702eb48c219278ea3b504e1ab5dac3c7116f7
dengqingfang <912552857@qq.com>
2018-07-24 14:43:39 +0800

    图片使用懒加载技术

--- src/main.js
+++ src/main.js
@@ -6,9 +6,16 @@ import Vue from 'vue'
 import App from './App'
 // router的实例
 import router from './router'
-import 'common/stylus/index.styl'
 // 解决移动端点击的300ms的延迟
 import fastclick from 'fastclick'
+import VueLazyLoad from 'vue-lazyload'
+
+import 'common/stylus/index.styl'
+
+Vue.use(VueLazyLoad, {
+  // loading: './common/image/default.png'
+  loading: require('common/image/default.png')
+})
 
 Vue.config.productionTip = false
 fastclick.attach(document.body)

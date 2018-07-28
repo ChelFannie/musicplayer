777e3aafb03359e4687fa10399fb42813e94053a
dengqingfang <912552857@qq.com>
2018-07-28 21:16:43 +0800

    完成歌手列表数据的抓取和获取

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -5,21 +5,35 @@
 </template>
 
 <script>
+import {getSingerList} from 'api/singer.js'
+import {ERR_OK} from 'api/config.js'
 export default {
   name: 'singer',
   data () {
     return {
-      msg: 'singer'
+      msg: 'singer',
+      singers: []
     }
   },
   created () {
-
+    this._getSingerList()
   },
   methods: {
-
+    _getSingerList () {
+      getSingerList().then(res => {
+        if (res.code === ERR_OK) {
+          this.singers = res.data.list
+        }
+      })
+    }
   }
 }
 </script>
 
-<style>
+<style lang="stylus" rel="stylesheet/stylus">
+.singer
+  position fixed
+  top 88px
+  bottom 0
+  width 100%
 </style>

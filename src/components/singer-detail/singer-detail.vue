5e33c7945fd66106bc2f5d8491897919b071b238
dengqingfang <912552857@qq.com>
2018-07-31 15:25:13 +0800

    给子路由添加过度动画

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -1,7 +1,9 @@
 <template>
- <div class="singer-detail">
+  <transition name="slide">
+    <div class="singer-detail">
 
- </div>
+    </div>
+  </transition>
 </template>
 
 <script>
@@ -30,4 +32,10 @@ export default {
   right 0
   bottom 0
   background $color-background
+.slide-enter-active, .slide-leave-active{
+  transition all 0.3s
+}
+.slide-enter, .slide-leave-to{
+  transform translate3d(100%, 0, 0)
+}
 </style>

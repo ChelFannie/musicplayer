364c74eae083f6578445e6871044e2f6986500b8
dengqingfang <912552857@qq.com>
2018-08-06 11:15:39 +0800

    简单完成播放器布局

--- src/App.vue
+++ src/App.vue
@@ -5,17 +5,21 @@
     <keep-alive>
       <router-view></router-view>
     </keep-alive>
+    <!-- 因为在每个页面都能播放 -->
+    <player></player>
   </div>
 </template>
 
 <script>
 import MHeader from 'components/m-header/m-header'
 import Tab from 'components/tab/tab'
+import Player from './components/player/player'
 export default {
   name: 'App',
   components: {
     MHeader,
-    Tab
+    Tab,
+    Player
   }
 }
 </script>
